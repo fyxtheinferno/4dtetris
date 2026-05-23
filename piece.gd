68092b72451804f80d81b4edf9a3c9a2d3b3c119
fyxtheinferno <54044047+fyxtheinferno@users.noreply.github.com>
2026-05-23 00:27:25 -0500

    rudimentary bits for constructing the play board

---
+++ piece.gd
@@ -0,0 +1,15 @@
+extends Node3D
+@export var block1: MeshInstance3D
+@export var block2: MeshInstance3D
+@export var block3: MeshInstance3D
+@export var block4: MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
