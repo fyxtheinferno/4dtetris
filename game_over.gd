f0f583081ef3453204998a9edb6f3e9212ff8fab
fyxtheinferno <54044047+fyxtheinferno@users.noreply.github.com>
2026-06-23 23:48:49 -0500

    retry is functional

--- game_over.gd
+++ game_over.gd
@@ -6,3 +6,8 @@ func _ready():
 
 func _on_pc_spawner_game_over() -> void:
 	show()
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and visible:
+		# This restarts the current scene.
+		get_tree().reload_current_scene()
